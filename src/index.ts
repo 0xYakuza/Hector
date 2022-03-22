bbbc7d7719ed98cfc11a25a295e75061ee184977
AzerothA <telifesite@gmail.com>
2022-03-23 03:16:35 +0430

    First commit

---
+++ src/index.ts
@@ -0,0 +1,20 @@
+import { Application } from "./application";
+import { Metamask } from "./wallets/metamask";
+import { log, Status } from "./log";
+import { SolCompiler, CompileConfig } from "./compiler";
+
+export {
+    // Application
+    Application,
+
+    // Wallets
+    Metamask,
+
+    // Compiler
+    SolCompiler,
+    CompileConfig,
+
+    // log
+    log,
+    Status,
+}
